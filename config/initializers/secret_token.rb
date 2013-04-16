e3f6d70adc860cb889ebf6af71d3c5c4c5f1e706
Jesse Buchanan <jbinto@jbinto.ca>
2013-04-16 09:43:30 -0400

    rails new

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Puckpicks::Application.config.secret_token = '8f6f65d8fb8927a49166c02aad11602374835ffe2acac45079934bd8c13cd07da5ba6344c27886cc892651a305144a620abcda32968a60d9e23bb0cb86bde90b'
