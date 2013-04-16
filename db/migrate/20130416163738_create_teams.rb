89e1006f1703c38e14d63564dbac39ca1a76e4e3
Jesse Buchanan <jbinto@jbinto.ca>
2013-04-16 12:45:41 -0400

    Add team model

---
+++ db/migrate/20130416163738_create_teams.rb
@@ -0,0 +1,12 @@
+class CreateTeams < ActiveRecord::Migration
+  def change
+    create_table :teams do |t|
+      t.string :code
+      t.string :city
+      t.string :name
+      t.string :logo
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,16 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130416140702) do
+ActiveRecord::Schema.define(:version => 20130416163738) do
+
+  create_table "teams", :force => true do |t|
+    t.string   "code"
+    t.string   "city"
+    t.string   "name"
+    t.string   "logo"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
 
   create_table "users", :force => true do |t|
     t.string   "username",         :null => false
