0c3cd491a56399b85e71cec52ab33aae33929d7a
Jesse Buchanan <jbinto@jbinto.ca>
2013-04-17 16:11:30 -0400

    Add more validations to Game and Pick.
    * Game: setting result finishes game.
    * Pick: finished game requires scores & winner.

--- app/models/game.rb
+++ app/models/game.rb
@@ -8,12 +8,31 @@ class Game < ActiveRecord::Base
   belongs_to :away, :class_name => "Team"
   belongs_to :winner, :class_name => "Team"
 
-  validate :cannot_be_same_teams
+  #validate :cannot_be_same_teams
+
+  validates :away, :exclusion => { :in => lambda { |game| [game.home] } }
+
+  validates :home, :presence => true
+  validates :away, :presence => true
+  validates :faceoff_time, :presence => true
+
+  validates :winner, :presence => true, :if => :finished?
+  validates :home_score, :presence => true, :if => :finished?
+  validates :away_score, :presence => true, :if => :finished?
 
   def cannot_be_same_teams
     if home == away
       errors.add(:away, "can't be same as home")
       errors.add(:home, "can't be same as away")
     end
+  end
+
+  # Thought: Rather than using attr_accessible, we can use a 
+  # special method here not accessible to the general public?
+  def set_result(opts)
+    self.winner = opts[:winner]
+    self.home_score = opts[:home_score]
+    self.away_score = opts[:away_score]
+    self.finished = true
   end
 end
--- test/unit/game_test.rb
+++ test/unit/game_test.rb
@@ -7,5 +7,22 @@ class GameTest < ActiveSupport::TestCase
     refute game.valid?
   end
 
+  test "setting result finishes the game" do
+    game = FactoryGirl.build(:game)
+    game.set_result(winner: game.home, home_score: 5, away_score: 2)
+    assert game.finished?
+  end
+
+  test "finished game must have scores and a winner" do
+    game = FactoryGirl.build(:game, :finished => true)
+    refute game.valid?
+
+    assert game.errors[:winner].count == 1
+    assert game.errors[:home_score].count == 1
+    assert game.errors[:away_score].count == 1
+  end
+
+  test "winning team must be a team that played" do
+  end
 
 end
