1fc57f1ac4461e161784ad43093f5ec616c171ba
Jesse Buchanan <jbinto@jbinto.ca>
2013-04-18 19:35:47 -0400

    Make helpers for strftime calls

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+    def today_text
+      date_only(Time.zone.today)
+    end
+
+    def date_only(date)
+      date.strftime('%A, %b %d')
+    end
+
+    def time_only(date)
+      date.localtime.strftime('%l:%M %p')
+    end
 end
