cb461f1c5cc670a4627534a5f6257ff911118e12
Jesse Buchanan <jbinto@jbinto.ca>
2013-04-19 09:15:49 -0400

    Use current time

--- lib/now.rb
+++ lib/now.rb
@@ -1,16 +1,27 @@
 class Now
+  MOCK = false
+
   def self.now
     # Time.zone.now => Thu, 18 Apr 2013 23:07:53 EDT -04:00
     # Time.zone.now.class => ActiveSupport::TimeWithZone
 
-    Time.zone.parse('2013-04-18 15:00:00')
+    if MOCK
+      Time.zone.parse('2013-04-18 15:00:00')
+    else
+      Time.zone.now
+    end
   end
 
   def self.today
     # Time.zone.today => Thu, 18 Apr 2013 (Date)
     # Time.zone.now.class => ActiveSupport::TimeWithZone
 
-    Date.parse('2013-04-18')
+    if MOCK
+      Date.parse('2013-04-18')
+    else
+      Time.zone.today
+    end
+
   end
 
 end
