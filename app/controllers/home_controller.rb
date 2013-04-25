984d72f9c41e93955999c683428946fca82f7801
Jesse Buchanan <jbinto@jbinto.ca>
2013-04-25 14:00:37 -0400

    Fix N+1 query problem on home page (Game->Team)

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,7 @@
 class HomeController < ApplicationController
 
   def home
-    @today_games = Game.today.order(:id).all
+    @today_games = Game.includes(:home).includes(:away).today.order(:id).all
     if logged_in?
       @pick = current_user.picks.build
       @my_picks = current_user.picks
