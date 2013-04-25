af48fcd664194fb33ea6ba67252ff14a32eccf44
Jesse Buchanan <jbinto@jbinto.ca>
2013-04-25 14:33:46 -0400

    N+1's everywhere. current_user.picks must include game.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,7 +4,7 @@ class HomeController < ApplicationController
     @today_games = Game.includes(:home).includes(:away).today.order(:id).all
     if logged_in?
       @pick = current_user.picks.build
-      @my_picks = current_user.picks
+      @my_picks = current_user.picks.includes(:game).includes(:team)
     end
   end
 
