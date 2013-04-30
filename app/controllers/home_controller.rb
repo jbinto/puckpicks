6813630c21bdc09b33d77f7a10d84c395c3d9275
Jesse Buchanan <jbinto@jbinto.ca>
2013-04-30 18:49:40 -0400

    Implement leaderboards. Show on front page.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,7 @@ class HomeController < ApplicationController
 
   def home
     @today_games = Game.includes(:home).includes(:away).today.order(:id).all
+    @leaders = User.leaders
     if logged_in?
       @pick = current_user.picks.build
       @my_picks = current_user.picks.includes(:game).includes(:team)
--- app/models/user.rb
+++ app/models/user.rb
@@ -16,4 +16,29 @@ class User < ActiveRecord::Base
   def score
     picks.sum(:impact)
   end
+
+  def self.leaders
+    leaders = User.joins(:picks).select("users.username, sum(impact) as points").group("users.username").order("points desc").all
+    
+    position = 0
+    leaders.map { |l| {position: position+=1, username: l.username, points: l.points } }
+    
+    # Calculating leaderboards is a bit of a challenge. My thoughts:
+
+    # 1) The naive approach is to just iterate through User.all and call the score method, but this is inefficient, it will do an N+1 query.:
+      # SELECT * FROM USERS
+      # SELECT SUM(impact) FROM PICKS WHERE USER_ID=1
+      # SELECT SUM(impact) FROM PICKS WHERE USER_ID=2
+      # ....
+
+    # 2) The code above works, but it's sketchy. It drops too deep into SQL for my liking. It also returns an ActiveRecord User, which is
+    #    totally misleading. Only the username attribute is selected, so everything else is nil. It also means I can't call the "score" 
+    #    field "score", since there's already a method by that name! Thus "points".  To work around the "returns a not-quite-User-object"
+    #    problem, I just map it into an array of hashes.
+
+    # 3) This is completely untestable and 100% coupled to my database. Is that okay? What if I wanted to introduce tiebreakers (e.g.
+    #    the user with the more correct picks regardless of spread). It's probably possible with juggling SQL, but how do I unit test
+    #    this?
+
+  end
 end
