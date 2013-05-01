3074994ab4632e741898343dbfb29d8366f335ee
Jesse Buchanan <jbinto@jbinto.ca>
2013-05-01 14:38:48 -0400

    Make username case insensitive when logging in

--- test/integration/session_flows_test.rb
+++ test/integration/session_flows_test.rb
@@ -7,23 +7,38 @@ class SessionFlowsTest < ActionDispatch::IntegrationTest
 
     perform_login('jdoe', 'abc123')
 
-    assert_equal root_path, current_path
-    assert page.has_content? 'Log out (jdoe)'
+    assert_was_logged_in
   end
 
   test "unsuccessful authentication" do
     perform_login('jdoe', 'abc123')
 
-    assert_equal '/login', current_path
-    assert page.has_content? 'password was invalid'
+    assert_was_not_logged_in
   end
-  
+
+  test "username is case insensitive when authenticating" do
+    FactoryGirl.create(:user, :username => "JDoe", :password => "abc123")
+    perform_login('jdoe', 'abc123')
+
+    assert_was_logged_in
+  end
+
   protected
   def perform_login user, password
     visit '/login'
     fill_in 'login[username]', with: user
     fill_in 'login[password]', with: password
     click_button 'Log in'
+  end
+
+  def assert_was_logged_in
+    assert_equal root_path, current_path
+    assert page.has_content? 'Log out'
+  end
+
+  def assert_was_not_logged_in
+    assert_equal '/login', current_path
+    assert page.has_content? 'password was invalid'
   end
 
 end
