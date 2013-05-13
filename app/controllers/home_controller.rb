0be2c4e14ae381cdb1f57cda830c6af1c208c5c9
Jesse Buchanan <jbinto@jbinto.ca>
2013-05-13 08:24:53 -0400

    Order games by start time. Fix issue #10.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,7 @@
 class HomeController < ApplicationController
 
   def home
-    @today_games = Game.includes(:home).includes(:away).today.order(:id).all
+    @today_games = Game.includes(:home).includes(:away).today.order(:faceoff_time).all
     @leaders = User.leaders
     if logged_in?
       @pick = current_user.picks.build
